4fe722db0bdfedc29ebcd783f90e74a68620928e
Muhammed Can Kırkgöz <73443107+cankirkgz@users.noreply.github.com>
2023-06-04 19:23:27 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz: ");
+        int n = scanner.nextInt();
+
+        // Elmasın üst kısmı
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Elmasın alt kısmı
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
